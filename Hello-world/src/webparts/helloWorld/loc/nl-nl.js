42a84ef046aee1cee2f37b5e557b4153e08e6c22
cnanjappa <cnanjappa@yahoo.com>
2021-11-29 04:48:14 -0500

    first

---
+++ Hello-world/src/webparts/helloWorld/loc/nl-nl.js
@@ -0,0 +1,7 @@
+define([], function() {
+  return {
+    "PropertyPaneDescription": "Description in dutch",
+    "BasicGroupName": "Dutch Group Name",
+    "DescriptionFieldLabel": "Description Field in dutch"
+  }
+});
