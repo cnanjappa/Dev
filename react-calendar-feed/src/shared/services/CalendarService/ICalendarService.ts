851f8e55045e6843c2f49930eac43b9496bc264c
cnanjappa <cnanjappa@yahoo.com>
2022-12-19 18:52:34 -0500

    New Location

---
+++ react-calendar-feed/src/shared/services/CalendarService/ICalendarService.ts
@@ -0,0 +1,16 @@
+import { IWebPartContext } from "@microsoft/sp-webpart-base";
+import { CalendarEventRange, ICalendarEvent } from ".";
+
+export interface ICalendarService {
+    Context: IWebPartContext;
+    FeedUrl: string;
+    NavigationUrl: string;    
+    ArchiveUrl: string;
+    EventRange: CalendarEventRange;
+    UseCORS: boolean;
+    CacheDuration: number;
+    MaxTotal: number;
+    ConvertFromUTC: boolean;
+    Name: string;
+    getEvents: () => Promise<ICalendarEvent[]>;
+}
